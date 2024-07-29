02b843531803a81fa86817695ab627653fdc2307
Olga Liubimova <lov800@yandex.ru>
2024-07-29 09:52:08 +0300

    Создать приложение на React

--- app.js
+++ app.js
@@ -8,14 +8,17 @@ const { errors } = require('celebrate');
 
 const { NODE_ENV, PORT, DATABASE } = process.env;
 
-const app=express();
+const app = express();
 const router = require('./routes/index');
 const { cors } = require('./middlewares/cors');
 const { handleError } = require('./middlewares/handleError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { limiter } = require('./middlewares/limiter');
 
-mongoose.connect(NODE_ENV === 'production' ? DATABASE : 'mongodb://127.0.0.1:27017/flylady', { useNewUrlParser: true });
+mongoose.connect(
+  NODE_ENV === 'production' ? DATABASE : 'mongodb://127.0.0.1:27017/flylady',
+  { useNewUrlParser: true }
+);
 //mongoose.connect('mongodb://127.0.0.1:27017/flylady');
 
 app.use(helmet());
@@ -30,8 +33,6 @@ app.use(errorLogger);
 app.use(errors());
 app.use(handleError);
 
-app.listen(PORT || 3000, () => {
-  console.log(`App listening on port ${PORT || 3000},`);
+app.listen(PORT || 3001, () => {
+  console.log(`App listening on port ${PORT || 3001},`);
 });
-
-
