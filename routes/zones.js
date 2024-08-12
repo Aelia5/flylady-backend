848466cb244651b244661ddb7b230cad19c6f0fd
Olga Liubimova <lov800@yandex.ru>
2024-08-12 09:38:23 +0300

    Исправить работу с задачами

--- routes/zones.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { validateId, validateTask } = require('../middlewares/validate');
-
-const { createTask } = require('../controllers/zones');
-
-router.patch('/:id/new-task', validateId, validateTask, createTask);
-
-module.exports = router;
